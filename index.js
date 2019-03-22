703583cc6712ae2458da777e4168448a71a0d99d
Raine Revere <raineorshine@gmail.com>
2019-03-22 11:00:50 -0600

    Rename and refactor groupTransactions() into calculate(). Integrate cost basis calculations.

--- index.js
+++ index.js
@@ -113,14 +113,24 @@ const price = mockPrice ? async () => 0 : memoize(async (from, to, time) => {
 })
 
 const isUsdBuy = trade =>
-  (trade.Comment === 'Shift Card' || // shift card (explicit)
-  (trade.Type === 'Withdrawal' && trade.Exchange === 'Coinbase' && !trade.Fee && trade.Sell < 4) || // shift card (infer)
-  (trade.Type === 'Trade' && trade.CurBuy === 'USD')) && // USD Sale
+  ((trade.Type === 'Withdrawal' && trade.Exchange === 'Coinbase' && !trade.Fee && trade.Sell < 4) || // shift card (infer)
+  (trade.Type === 'Trade' && trade.CurBuy === 'USD')) && // Crypto Sale
   trade.CurSell !== 'USDT' // not tether
+
+// find a withdrawal in the given list of transactions that matches the given deposit
+const findMatchingWithdrawal = (deposit, txs) =>
+  txs.find(tx => match(deposit, tx))
+
+
+/****************************************************************
+* CALCULATE
+*****************************************************************/
 
 // group transactions into several broad categories
 // match same-day withdrawals and deposits
-const groupTransactions = trades => {
+// calculate custom cost basis
+const calculate = async txs => {
+
   const matched = []
   const unmatched = []
   const income = []
@@ -128,98 +138,150 @@ const groupTransactions = trades => {
   const withdrawals = []
   const margin = []
   const lending = []
-  const unmatchedRequests = [] // thunks for prices
   const tradeTxs = []
   const lost = []
   const spend = []
 
-  const txsByDay = groupByDay(trades)
+  let sales = []
+  let trades = []
+  const errors = []
+
+  const txsByDay = groupByDay(txs)
 
   // loop through each day
   for (let key in txsByDay) {
     const group = txsByDay[key]
 
-    // loop through each transaction
-    // label loop so that matching
-    txLoop:
+    // loop through each of the day's transactions
     for (let i in group) {
-      const tx1 = group[i]
+      const tx = group[i]
 
-      if(/lending/i.test(tx1['Trade Group']) || /lending/i.test(tx1.Comment)) {
-        lending.push(tx1)
-      }
-      else if(/margin/i.test(tx1['Trade Group']) || /margin/i.test(tx1.Comment)) {
-        margin.push(tx1)
-      }
-      else if(isUsdBuy(tx1)) {
-        usdBuys.push(tx1)
-      }
-      else if(tx1.Type === 'Trade') {
-        tradeTxs.push(tx1)
-      }
-      else if(tx1.Type === 'Income') {
-        income.push(tx1)
-      }
-      else if (tx1.Type === 'Withdrawal') {
-        withdrawals.push(tx1)
-      }
-      else if (tx1.Type === 'Lost') {
-        lost.push(tx1)
-      }
-      else if (tx1.Type === 'Spend') {
-        spend.push(tx1)
-      }
-      // try to match the deposit to a same-day withdrawal
-      else if (tx1.Type === 'Deposit') {
+      // LENDING
 
-        // loop through each other transaction on the same day to find a matching withdrawal
-        for (let i2 in group) {
-          const tx2 = group[i2]
-          if(match(tx1, tx2)) {
-            matched.push(tx1)
-            tx1.match = tx2
-            continue txLoop // jump to next tx
+      // must go ahead of Trade
+      if(/lending/i.test(tx['Trade Group']) || /lending/i.test(tx.Comment)) {
+        lending.push(tx)
+      }
+
+      // MARGIN
+
+      else if(/margin/i.test(tx['Trade Group']) || /margin/i.test(tx.Comment)) {
+        margin.push(tx)
+      }
+
+      // SALE
+
+      // USD buy = crypto sale
+      // must go ahead of Trade and Withdrawal
+      else if(isUsdBuy(tx)) {
+        usdBuys.push(tx)
+
+        // update cost basis
+        try {
+          sales = sales.concat(stock.trade(+tx.Sell, tx.CurSell, +tx.Buy, tx.CurBuy, tx['Trade Date']))
+        }
+        catch (e) {
+          if (e instanceof Stock.NoAvailablePurchaseError) {
+            console.error(e.message)
+            errors.push(e)
+          }
+          else {
+            throw e
+          }
+        }
+      }
+
+      // TRADE
+
+      // crypto-to-crypto trade
+      else if(tx.Type === 'Trade') {
+        tradeTxs.push(tx)
+
+        // update cost basis
+        try {
+          const tradeExchanges = stock.trade(+tx.Sell, tx.CurSell, +tx.Buy, tx.CurBuy, tx['Trade Date'])
+          trades = trades.concat(tradeExchanges)
+        }
+        catch (e) {
+          if (e instanceof Stock.NoAvailablePurchaseError) {
+            console.error('Error making trade:', e.message)
+            errors.push(e)
+          }
+          else {
+            throw e
           }
         }
 
-        // otherwise we have an unmatched transaction
-        delete tx1.field1
+      }
+      else if(tx.Type === 'Income') {
+        income.push(tx)
 
-        const newTx = Object.assign({}, tx1, {
-          Type: 'Income',
-          Comment: 'Cost Basis',
-          Group: ''
-        })
+        // update cost basis
+        let p = 0
+        try {
+          p = await price(tx.CurBuy, 'USD', day(normalDate(tx)))
+        }
+        catch(e) {
+            console.error(`Error fetching price`)
+            errors.push(tx)
+          }
+        stock.deposit(+tx.Buy, tx.CurBuy, tx.Buy * p, tx['Trade Date'])
+      }
 
-        if (command === 'prices') {
-          unmatchedRequests.push(async () => {
-            let p, err
-            try {
-              // per-day memoization
-              p = await price(tx1.CurBuy, 'USD', day(normalDate(tx1)))
-            }
-            catch (e) {
-              err = e.message
-            }
+      // DEPOSIT
 
-            return {
-              tx: Object.assign({}, newTx, { Price: p }),
-              error: err
-            }
+      else if (tx.Type === 'Deposit') {
+
+        // try to match the deposit to a same-day withdrawal
+        if (findMatchingWithdrawal(tx, group)) {
+          matched.push(tx)
+        }
+        // otherwise we have an unmatched transaction and need to fallback to the day-of price
+        // and add it to the stock
+        else {
+          console.warn(`WARNING: No matching withdrawal for deposit of ${tx.Buy} ${tx.CurBuy} on ${tx['Trade Date']}. Using historical price.`)
+
+          let p
+          try {
+            // per-day memoization
+            p = await price(tx.CurBuy, 'USD', day(normalDate(tx)))
+          }
+          catch (e) {
+            errors.push(e.message)
+          }
+
+          const newTx = Object.assign({}, tx, {
+            Type: 'Income',
+            Comment: 'Cost Basis',
+            Price: p
           })
-        }
-        else if (command !== 'sample' || unmatched.length < sampleSize) {
-        // ignore txs beyond sampleSize
+
           unmatched.push(newTx)
+
+          // update cost basis
+          stock.deposit(+tx.Buy, tx.CurBuy, tx.Buy * p, tx['Trade Date'])
         }
+
+      }
+
+      // OTHER
+
+      else if (tx.Type === 'Withdrawal') {
+        withdrawals.push(tx)
+      }
+      else if (tx.Type === 'Lost') {
+        lost.push(tx)
+      }
+      else if (tx.Type === 'Spend') {
+        spend.push(tx)
       }
       else {
-        throw new Error('I do not know how to handle this transaction: \n\n' + JSON.stringify(tx1))
+        throw new Error('I do not know how to handle this transaction: \n\n' + JSON.stringify(tx))
       }
     }
   }
 
-  return { matched, unmatched, income, usdBuys, withdrawals, tradeTxs, lost, spend, margin, lending, unmatchedRequests }
+  return { matched, unmatched, income, usdBuys, withdrawals, tradeTxs, lost, spend, margin, lending, sales, trades, errors }
 }
 
 
@@ -231,8 +293,8 @@ const file = process.argv[2]
 const command = process.argv[3]
 const sampleSize = process.argv[4] || Infinity
 
-if (!file || !command) {
-  console.error('Invalid usage. \n\nUsage: \nnode index.js [command] [transactions.csv]')
+if (!file) {
+  console.error('Please specify a file. \n\nUsage: \nnode index.js [transactions.csv] command')
   process.exit(1)
 }
 
@@ -241,223 +303,43 @@ if (!file || !command) {
 (async () => {
 
 const input = fixHeader(fs.readFileSync(file, 'utf-8'))
-const rawData = Array.prototype.slice.call(await csvtojson().fromString(input)) // convert to true array
+const txs = Array.prototype.slice.call(await csvtojson().fromString(input)) // convert to true array
 
-const { matched, unmatched, income, usdBuys, lost, spend, withdrawals, tradeTxs, margin, lending, unmatchedRequests } = groupTransactions(rawData)
+const { matched, unmatched, income, usdBuys, lost, spend, withdrawals, tradeTxs, margin, lending, sales, trades, errors } = await calculate(txs)
 
 
 /************************************************************************
  * SUMMARY
  ************************************************************************/
 if (command === 'summary') {
-  console.log('Transactions:', rawData.length)
-  console.log('Margin Trades:', margin.length)
-  console.log('Lending:', lending.length)
-  console.log('USD Buys:', usdBuys.length)
-  console.log('Income:', income.length)
-  console.log('Lost:', lost.length)
-  console.log('Spend:', spend.length)
-  console.log('Trades:', tradeTxs.length)
+
+  const sum = withdrawals.length + matched.length + unmatched.length + usdBuys.length + income.length + tradeTxs.length + margin.length + lending.length + lost.length + spend.length
+
+  console.log('')
   console.log('Withdrawals:', withdrawals.length)
   console.log('Matched Deposits:', matched.length)
   console.log('Unmatched Deposits:', unmatched.length)
-}
+  console.log('USD Buys:', usdBuys.length)
+  console.log('Income:', income.length)
+  console.log('Trades:', tradeTxs.length)
+  console.log('Margin Trades:', margin.length)
+  console.log('Lending:', lending.length)
+  console.log('Lost:', lost.length)
+  console.log('Spend:', spend.length)
+  console.log(sum === txs.length
+    ? `TOTAL: ${sum} ✓`
+    : `✗ TOTAL: ${sum}, TXS: ${txs.length}`
+  )
+  console.log('')
 
+  const stockSum = trades.length + sales.length
+  console.log('Trades', trades.length)
+  console.log('Sales', sales.length)
+  console.log(`SUM: ${stockSum}`)
+  console.log('')
 
-/************************************************************************
- * COST BASIS
- ************************************************************************/
-else if (command === 'costbasis') {
-
-  const n = Math.min(rawData.length, sampleSize)
-  const errors = []
-  let withdrawals = []
-  let deposits = []
-  let trades = []
-  let sales = []
-  let income = []
-  let lost = []
-  let spend = []
-
-  for (let i=0; i<n; i++) {
-    const tx = rawData[i]
-
-    // ignore "Cost Basis" transactions which were added for the CoinTracking tax report
-    // they are duplicates to unmatched deposits, which we can derive the cost basis of
-    if (tx.Comment === 'Cost Basis') {
-      continue
-    }
-
-    // if (tx.CurBuy) {
-    //   console.log("balance", tx.CurBuy, stock.balance(tx.CurBuy))
-    // }
-    // if (tx.CurSell) {
-    //   console.log("balance", tx.CurSell, stock.balance(tx.CurSell))
-    // }
-    // console.log("tx", tx)
-    switch(tx.Type) {
-
-      // ignore ALL withdrawals, as they do not affect cost basis calculations
-      case 'Withdrawal':
-        withdrawals.push(tx)
-      //   // ignore USD withdrawals
-      //   // ignore transactions processed via an out-of-order deposit
-      //   if (tx.CurSell !== 'USD' && !tx.processed) {
-      //     // store the cost basis (1 or more) of the withdrawal in the Withdrawal tx object so that the matching Deposit tx has access to the cost basis
-      //     try {
-      //       tx.processed = true
-      //       tx.withdrawals = stock.withdraw(+tx.Sell, tx.CurSell, tx['Trade Date'])
-      //     }
-      //     catch (e) {
-      //       if (e instanceof Stock.NoAvailablePurchaseError) {
-      //         console.error('Error making withdrawal:', e.message)
-      //         errors.push(e)
-      //       }
-      //       else {
-      //         throw e
-      //       }
-      //     }
-      //   }
-      //   // TODO: process pending deposit
-        break
-
-      case 'Deposit':
-
-        deposits.push(tx)
-
-        // USD cost basis = buy amount
-        if (tx.CurBuy === 'USD') {
-          // stock.deposit(+tx.Buy, tx.CurBuy, +tx.Buy, tx['Trade Date'])
-          // ignore USD deposits
-        }
-        // get the cost basis from matching deposits
-        else if (tx.match) {
-
-          // if we get a deposit before its matching withdrawal, go ahead and process the withdrawal now
-          // if (!tx.match.processed) {
-            // console.log("Processing out-of-order deposit with", tx.match)
-            try {
-              tx.match.processed = true
-              tx.match.withdrawals = stock.withdraw(+tx.match.Sell, tx.match.CurSell, tx.match['Trade Date'])
-            }
-            catch (e) {
-              if (e instanceof Stock.NoAvailablePurchaseError) {
-                console.error(e.message)
-                errors.push(e)
-              }
-              else {
-                throw e
-              }
-            }
-          // }
-
-          if (tx.match.withdrawals) {
-            tx.match.withdrawals.forEach(withdrawal => {
-              stock.deposit(withdrawal.amount, withdrawal.cur, withdrawal.cost, withdrawal.date)
-            })
-          }
-          else {
-            console.error(`ERROR: Matching deposit of ${tx.Buy} ${tx.CurBuy} on ${tx['Trade Date']} with no withdrawals.`)
-            errors.push(tx)
-          }
-        }
-        else {
-          console.warn(`WARNING: No matching withdrawal for deposit of ${tx.Buy} ${tx.CurBuy} on ${tx['Trade Date']}. Using historical price.`)
-          let p = 0
-          try {
-            p = await price(tx.CurBuy, 'USD', day(normalDate(tx)))
-          }
-          catch(e) {
-            console.error(`Error fetching price`)
-            errors.push(tx)
-          }
-          stock.deposit(+tx.Buy, tx.CurBuy, tx.Buy * p, tx['Trade Date'])
-          // errors.push('No matching withdrawal for deposit')
-        }
-
-        break
-
-      case 'Trade':
-        try {
-          const tradeExchanges = stock.trade(+tx.Sell, tx.CurSell, +tx.Buy, tx.CurBuy, tx['Trade Date'])
-          if (tx.CurBuy === 'USD') {
-            sales = sales.concat(tradeExchanges)
-          }
-          else {
-            trades = trades.concat(tradeExchanges)
-          }
-        }
-        catch (e) {
-          if (e instanceof Stock.NoAvailablePurchaseError) {
-            console.error('Error making trade:', e.message)
-            errors.push(e)
-          }
-          else {
-            throw e
-          }
-        }
-        break
-
-      case 'Income':
-        let p = 0
-        try {
-          p = await price(tx.CurBuy, 'USD', day(normalDate(tx)))
-        }
-        catch(e) {
-            console.error(`Error fetching price`)
-            errors.push(tx)
-          }
-        income = income.concat(stock.deposit(+tx.Buy, tx.CurBuy, tx.Buy * p, tx['Trade Date']))
-        break
-
-      case 'Lost':
-        lost.push(tx)
-        break
-
-      case 'Spend':
-        spend.push(tx)
-        break
-
-      default:
-        throw new Error('Unknown transaction type: \n' + JSON.stringify(tx))
-    }
-  }
-
-  console.log('withdrawals', withdrawals.length)
-  console.log('deposits', deposits.length)
-  console.log('trades', trades.length)
-  console.log('sales', sales.length)
-  console.log('income', income.length)
-  console.log('lost', lost.length)
-  console.log('spend:', spend.length)
-  console.log('errors', errors.length)
-}
-
-//  prices
-else if (command === 'prices') {
-
-  const errors = []
-
-  const numRequests = Math.min(unmatchedRequests.length, sampleSize)
-  const bar = new ProgressBar(':current/:total :percent :etas (:token1 errors)', { total: numRequests })
-  bar.render()
-  for (let i=0; i<numRequests; i++) {
-    const result = await unmatchedRequests[i]()
-    if (!result.error) {
-      unmatched.push(result.tx)
-    }
-    else {
-      errors.push(result.error)
-    }
-    bar.tick({ token1: errors.length })
-  }
-
-  if (errors.length > 0) {
-    console.warn(errors.join('\n'))
-  }
-
-  // output
-  console.log(toCSV(unmatched))
+  console.error('ERRORS')
+  errors.forEach(err => console.error(err.message))
 }
 
 })()
